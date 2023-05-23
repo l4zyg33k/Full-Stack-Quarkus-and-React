4bec5ae85938f0915ee678ca87a104dd092215fd
l4zyg33k <lazygeek@miralove.pe.kr>
2023-05-23 23:22:49 +0900

    commit

--- src/main/java/com/example/fullstack/user/UserService.java
+++ src/main/java/com/example/fullstack/user/UserService.java
@@ -6,12 +6,25 @@ import io.quarkus.elytron.security.common.BcryptUtil;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.hibernate.ObjectNotFoundException;
 
 import java.util.List;
 
 @ApplicationScoped
 public class UserService {
+
+    private final JsonWebToken jwt;
+
+    @Inject
+    public UserService(JsonWebToken jwt) {
+        this.jwt = jwt;
+    }
+
+    public static boolean matchs(User user, String password) {
+        return BcryptUtil.matches(password, user.password);
+    }
 
     public Uni<User> findById(long id) {
         return User.<User>findById(id).onItem().ifNull().failWith(() -> new ObjectNotFoundException(id, "User"));
@@ -33,7 +46,10 @@ public class UserService {
 
     @WithTransaction
     public Uni<User> update(User user) {
-        return findById(user.id).chain(u -> User.getSession()).chain(s -> s.merge(user));
+        return findById(user.id).chain(u -> {
+            user.password = u.password;
+            return User.getSession();
+        }).chain(s -> s.merge(user));
     }
 
     @WithTransaction
@@ -42,10 +58,6 @@ public class UserService {
     }
 
     public Uni<User> getCurrentUser() {
-        return User.find("order by ID").firstResult();
-    }
-
-    public static boolean matchs(User user, String password) {
-        return BcryptUtil.matches(password, user.password);
+        return findByName(jwt.getName());
     }
 }
